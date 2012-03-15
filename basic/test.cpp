1643843d441b52115558ac640451fd0d57769eda
gang <gang@gang-PC.(none)>
2012-03-15 16:48:55 +0800

    test default argument

--- basic/test.cpp
+++ basic/test.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 #include <cstdio>
 using namespace std;
-
-int func(int, int);
-int main()
+#define FIELD(a) char* a##_string
+class Test
 {
-	func(10, 10);
+public:
+	void func(int a, int i = 0);
+};
+
+void Test::func(int a, int i)
+{
+	cout << "In Test i = " << i << endl;
 }
 
-int func(int a, int)
+int func(int a, int i = 0);
+
+int main()
 {
-	printf("Hello %d\n", a);
+	char a[] = "Fenggang\n";
+	FIELD(one);
+	one_string =  a;
+	cout << one_string << endl;
+	func(10, 1);
+	func(10);
+	Test t;
+	t.func(10);
+	t.func(12, 2);
+}
+int func(int a, int i)
+{
+	printf("Hello i = %d\n", i);
 }
